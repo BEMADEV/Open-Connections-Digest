bd664ab0fabb49aaf5a356e3280e64d03dc6afb2
Taylor Cavaletto <tjcavaletto@gmail.com>
2023-08-08 16:19:53 -0400

    - Added Open Connections Digest

---
+++ com.bemaservices.OpenConnectionsDigest/SystemGuid/SystemCommunication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.bemaservices.OpenConnectionsDigest.SystemGuid
+{
+    public static class SystemCommunication
+    {
+        /// <summary>
+        /// The open connects digest system email Guid
+        /// </summary>
+        public static string OPEN_CONNECTS_DIGEST = "F3694629-0C5E-4B13-BDCD-45B21480CC84";
+    }
+}
